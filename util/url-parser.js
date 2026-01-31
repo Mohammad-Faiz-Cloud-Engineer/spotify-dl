c292cce52c99768b087b3d57265095c1bc95635c
Mohammad Faiz <mohdfaiz2k9@gmail.com>
2026-01-31 21:10:57 +0530

    Update url-parser.js

--- util/url-parser.js
+++ util/url-parser.js
@@ -1,19 +1,43 @@
+/**
+ * Parses Spotify/YouTube URL and determines content type
+ * @param {string} inputUrl - URL to parse
+ * @returns {string} Content type identifier
+ * @throws {Error} If URL format is invalid
+ */
 export default inputUrl => {
-  if (inputUrl.includes('youtube')) {
-    return 'youtube';
-  } else if (inputUrl.includes('/track/')) {
-    return 'song';
-  } else if (inputUrl.includes('/playlist/')) {
-    return 'playlist';
-  } else if (inputUrl.includes('/album/')) {
-    return 'album';
-  } else if (inputUrl.includes('/artist/')) {
-    return 'artist';
-  } else if (inputUrl.includes('/show/')) {
-    return 'show';
-  } else if (inputUrl.includes('/episode/')) {
-    return 'episode';
-  } else {
-    return new Error('Invalid spotify URL');
+  if (!inputUrl || typeof inputUrl !== 'string') {
+    throw new Error('Invalid URL: URL must be a non-empty string');
   }
+
+  const urlLower = inputUrl.toLowerCase();
+
+  if (urlLower.includes('youtube')) {
+    return 'youtube';
+  }
+  
+  if (urlLower.includes('/track/')) {
+    return 'song';
+  }
+  
+  if (urlLower.includes('/playlist/')) {
+    return 'playlist';
+  }
+  
+  if (urlLower.includes('/album/')) {
+    return 'album';
+  }
+  
+  if (urlLower.includes('/artist/')) {
+    return 'artist';
+  }
+  
+  if (urlLower.includes('/show/')) {
+    return 'show';
+  }
+  
+  if (urlLower.includes('/episode/')) {
+    return 'episode';
+  }
+
+  throw new Error('Invalid URL: Unsupported Spotify or YouTube URL format');
 };
